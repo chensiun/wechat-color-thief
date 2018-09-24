776d8633c576143b710842d8dfa47dc45605dc3a
chensiun <chensiun1008@gmail.com>
2018-09-24 11:18:02 +0800

    [wip] try basic work

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,11 @@
 //index.js
 //获取应用实例
 const app = getApp()
+//const colorThief = require('./ColorThief').default
+import ColorThief from './ColorThief.js'
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,39 +14,39 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    this.colorThief = new ColorThief()
+
+    // var tt = wx.canvasGetImageData({
+    //   canvasId: 'canvasPlaceholder',
+    //   x: 0,
+    //   y: 0,
+    //   width: 100,
+    //   height: 100,
+    // })
+    // // var context = wx.createCanvasContext('canvasPlaceholder')
+    // console.log('xxx...', tt)
+  },
+
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          photoSrc: res.tempImagePath
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  photoError(e) {
+    console.log(e.detail)
+  },
+
+  onThiefColor() {
+    const src = this.data.photoSrc
+    this.colorThief.getColor(src).then(data => {
+      const rgb = this.colorThief.convertColorRgb(data)
+      this.setData({ mainColor: rgb })
     })
   }
 })
